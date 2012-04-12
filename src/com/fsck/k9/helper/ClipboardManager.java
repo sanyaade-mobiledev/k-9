d3bf482db871953f13bf6a2b7f498f13ecfc1d3e
kris kechagia <kk@rndsec.net>
2012-04-12 13:24:51 +0200

    disabled ClipboardManagerApi11, wont build on gingerbread, thx cgetti!
    Change-Id: I35a6afb647cc3eb44220473483640c8397bb3983

--- src/com/fsck/k9/helper/ClipboardManager.java
+++ src/com/fsck/k9/helper/ClipboardManager.java
@@ -28,11 +28,7 @@ public abstract class ClipboardManager {
         Context appContext = context.getApplicationContext();
 
         if (sInstance == null) {
-            if (Build.VERSION.SDK_INT < Build.VERSION_CODES.HONEYCOMB) {
-                sInstance = new ClipboardManagerApi1(appContext);
-            } else {
-                sInstance = new ClipboardManagerApi11(appContext);
-            }
+            sInstance = new ClipboardManagerApi1(appContext);
         }
 
         return sInstance;
